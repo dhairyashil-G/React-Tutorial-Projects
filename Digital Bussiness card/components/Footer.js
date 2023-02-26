ba39294d02d02320e580e2ba0af03ecbf0299cf9
Dhairyashil <dhairyasheel.ghatage@gmail.com>
2023-02-26 13:03:25 +0530

    direct upload

---
+++ Digital Bussiness card/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Footer(){
+    return(
+        <div className="footer-div">
+            <ul className="footer-list">
+                <li><img src="/src/Twitter-Icon.png"/></li>
+                <li><img src="/src/Facebook-Icon.png"/></li>
+                <li><img src="/src/Instagram-Icon.png"/></li>
+                <li><img src="/src/GitHub-Icon.png"/></li>
+            </ul>
+        </div>
+    )
+}
